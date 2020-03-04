73eab8fcf9912e471b9bb5f3a5c6c9a5677138ac
Danny Van Stemp <danwardvs@adsgames.net>
2020-03-04 12:33:38 -0500

    Displaying accelerometer and gyroscope data on app

--- Client/app/src/main/java/com/example/simorientclient/MainActivity.kt
+++ Client/app/src/main/java/com/example/simorientclient/MainActivity.kt
@@ -14,10 +14,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private var mSensorManager : SensorManager ?= null
     private var mAccelerometer : Sensor ?= null
+    private var mSensor : Sensor ?= null
 
-    val text_x = findViewById(R.id.text_x) as TextView
-    val text_y = findViewById(R.id.text_y) as TextView
-    val text_z = findViewById(R.id.text_z) as TextView
 
 
 
@@ -27,18 +25,47 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         // focus in accelerometer
         mAccelerometer = mSensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        mSensor = mSensorManager!!.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+
 
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
     }
 
+    override fun onResume() {
+        super.onResume()
+        mSensorManager!!.registerListener(this,mAccelerometer, SensorManager.SENSOR_DELAY_GAME)
+        mSensorManager!!.registerListener(this,mSensor, SensorManager.SENSOR_DELAY_GAME)
+
+    }
+
     override fun onSensorChanged(event: SensorEvent?) {
         if (event != null) {
-            text_x.text = event.values[0].toString();
-            text_y.text = event.values[1].toString();
-            text_z.text = event.values[2].toString();
+            if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
 
+                val accel_x = findViewById(R.id.accel_x) as TextView
+                val accel_y = findViewById(R.id.accel_y) as TextView
+                val accel_z = findViewById(R.id.accel_z) as TextView
+
+
+                accel_x.text = String.format("%.1f", event.values[0])
+                accel_y.text = String.format("%.1f", event.values[1])
+                accel_z.text = String.format("%.1f", event.values[2])
+
+            }
+            if (event.sensor.type == Sensor.TYPE_GYROSCOPE) {
+                val gyro_x = findViewById(R.id.gyro_x) as TextView
+                val gyro_y = findViewById(R.id.gyro_y) as TextView
+                val gyro_z = findViewById(R.id.gyro_z) as TextView
+
+
+                gyro_x.text = String.format("%.1f", event.values[0])
+                gyro_y.text = String.format("%.1f", event.values[1])
+                gyro_z.text = String.format("%.1f", event.values[2])
+
+
+            }
         }
     }
 
