ca3f5b24e3db5f932fa1b663c63f96da4b43acac
Danny Van Stemp <danwardvs@adsgames.net>
2020-03-04 20:32:31 -0500

    Unity project and panda3d example project

---
+++ Server/Assets/Server.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleTCP;
+
+public class Server : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello world!");
+        var server = new SimpleTcpServer().Start(5000);
+        server.DataReceived += (sender, msg) => {
+                msg.Reply("Content-Type: text/plain\n\nHello from my web server!"); 
+                };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
